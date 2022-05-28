5416ea0095aa6a28b04f51c0f0559d5e28c2fd9c
DorinaPetraru <dorinapetraruv@gmail.com>
2022-05-28 12:59:55 +0200

    last changes

---
+++ README.md
@@ -0,0 +1,23 @@
+<!--
+
+-   Adivina el color
+    Juego que te muestra un código de color en RGB y varias
+    cajas de color. Tienes que elegir la caja que representa el
+    color correcto.
+    Descripción
+    ● La aplicación debe mostrar la pantalla dividida en dos partes, en
+    una debe mostrar un código de color RGB (p.ej. 255, 87, 51). En la
+    segunda debe mostrar varias cajas de color.
+    ● Una de las cajas de color debe mostrar el color correcto, las otras
+    deben ser variaciones más o menos similares.
+    ● También se debe mostrar un contador de aciertos y fallos.
+    ● La persona jugadora tiene que hacer click en la caja de color
+    correcto.
+    ● Si se hace click en la caja correcta se acumula un punto en el
+    contador de aciertos y si se falla se acumula en el de fallos.
+    ● Seguidamente el juego debe mostrar un nuevo código de color y
+    nuevas cajas.
+    ● Se gana la partida cuando se llega a 3 aciertos y se pierde si se llega
+    a 3 fallos.
+    ● El juego tiene tres niveles , si aciertas 3 colores seguidos subes al siguiente nivel , con cada nivel aumenta los colores.
+->
---
+++ src.js
@@ -0,0 +1,226 @@
+'use strict';
+
+//selecionamos los nodos
+const body = document.querySelector('body');
+const table = document.querySelector('#table');
+const targetColor = document.querySelector('#targetColor');
+const startButton = document.querySelector('#startButton');
+const gameLevelDisplay = document.querySelector('#gameLevel');
+let modal = document.querySelector('#myModal');
+let scoreSpan = document.querySelector('#scoreSpan');
+let livesSpan = document.querySelector('#livesSpan');
+
+let score = 0;
+let lives = 10;
+let level = 1;
+
+const textGradient = document.querySelector('p.textGradient');
+textGradient.textContent = 'Adivina el color';
+
+//estilos css con js
+startButton.style.cssText = `
+            border-radius: 10px;
+            color: var(--whiteShadoww);
+            font-size: 1.5rem;
+            width: 90%;
+            margin: auto;
+            padding: auto;
+            text-align: center;
+          
+     `;
+
+gameLevelDisplay.style.cssText = `
+            display: flex;
+            justify-content: center;
+            align-item: center;
+            font-weight: 900;
+            margin: auto;
+            padding: auto;
+            text-align: center;
+            font-size: 8vmin;
+
+`;
+
+//función que genera colores aleatorios
+function getRandomColor() {
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+//función crear niveles
+function createBoard(level) {
+    if (level === 1) {
+        createTable(5, 5);
+    } else if (level === 2) {
+        createTable(7, 7);
+    } else if (level === 3) {
+        createTable(9, 9);
+    }
+    levelBackground();
+}
+
+function levelBackground() {
+    let levelOne = './videos/Balls - 104438.mp4';
+    let levelTwo = './videos/Abstract - 14668.mp4';
+    let levelThree = './videos/Day Of Birth - 13948.mp4';
+    let video = document.querySelector('body > video');
+    let source = document.createElement('source');
+
+    if (level === 1) {
+        source.removeAttribute(levelThree);
+        source.setAttribute('src', levelOne);
+    } else if (level === 2) {
+        source.removeAttribute(levelOne);
+        source.setAttribute('src', levelTwo);
+    } else if (level === 3) {
+        source.removeAttribute(levelTwo);
+        source.setAttribute('src', levelThree);
+    }
+    body.append(video);
+    video.append(source);
+}
+
+//función capturar color
+function colorPicker(element) {
+    let result;
+    result = element.style.background;
+    return result;
+}
+
+//función crear tabla
+function createTable(rows, columns) {
+    let target_x = Math.floor(Math.random() * columns);
+    let target_y = Math.floor(Math.random() * rows);
+
+    for (let y = 0; y < rows; y++) {
+        const tr = document.createElement('tr');
+
+        for (let x = 0; x < columns; x++) {
+            const td = document.createElement('td');
+
+            td.style.width = '50px';
+            td.style.height = '50px';
+
+            if (target_x === x && target_y === y) {
+                td.style.background = targetColor.innerHTML;
+            } else {
+                td.style.background = getRandomColor();
+            }
+
+            tr.append(td);
+        }
+        table.append(tr);
+    }
+}
+
+function handleClick(e) {
+    if (e.target.matches('td')) {
+        const td = e.target;
+        game(td);
+    }
+}
+
+// reiniciamos la table
+function tryGuess() {
+    table.innerHTML = '';
+    targetColor.innerHTML = getRandomColor();
+    targetColor.style.color = targetColor.innerHTML;
+    createBoard(level);
+    table.addEventListener('click', handleClick);
+}
+
+function updateScoreSpan(value) {
+    scoreSpan.innerHTML = value;
+}
+
+function updateLivesSpan(value) {
+    livesSpan.innerHTML = value;
+}
+
+function game(td) {
+    if (targetColor.innerHTML === colorPicker(td)) {
+        score++;
+        updateScoreSpan(score);
+        tryGuess();
+        if (score === 3 && level < 3) {
+            updateScoreSpan(score);
+            if (level < 3) {
+                level++;
+                /* startButton.style.background = '#191919';*/
+            }
+            winModal();
+        } else if (score === 3 && level === 3) {
+            updateScoreSpan(score);
+            level = 1;
+            finishGame();
+        }
+    } else {
+        if (lives - 1 > 0) {
+            lives--;
+            updateLivesSpan(lives);
+        } else if (lives - 1 === 0) {
+            lives--;
+            updateLivesSpan(lives);
+            level = 1;
+            loseModal();
+        }
+    }
+    console.log(`score: ${score}; lives: ${lives}`);
+}
+
+function presentation() {
+    modal.style.display = 'block';
+}
+function winModal() {
+    modal.style.display = 'inline-block';
+    startButton.textContent = `Start level ${level}`;
+    gameLevelDisplay.textContent = 'You win!!!';
+    gameLevelDisplay.style.color = getRandomColor();
+}
+
+function loseModal() {
+    modal.style.display = 'inline-block';
+    startButton.textContent = `Return to level ${level}`;
+    gameLevelDisplay.textContent = 'You lose!!!';
+    gameLevelDisplay.style.color = '#191919';
+
+    lives = 3;
+    score = 0;
+}
+
+function finishGame() {
+    modal.style.display = 'block';
+    startButton.textContent = `Start again at level: ${level}`;
+    gameLevelDisplay.textContent = 'You win and ended the game!!!';
+}
+
+function startGame() {
+    //Establecer puntuación inicial
+    modal.style.display = 'none';
+
+    //Actualizar texto puntuación
+    score = 0;
+    scoreSpan.textContent = score;
+
+    //Establecer las vidas iniciales
+
+    lives = 3;
+
+    //Actualizar texto vidas
+
+    livesSpan.textContent = lives;
+
+    //Generar tabla y colorTarget
+
+    tryGuess();
+}
+
+presentation();
+
+startButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+    startGame();
+});
